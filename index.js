645cf400dd5e960afc638d9600374b9cab52cc02
mrae43 <khaelsm.mrae43@gmail.com>
2026-02-09 20:41:35 +0800

    learn: query & mutations graphql (#1)
    * feat: query implement for allBooks & set script testing gql
    
    * feat: query implement for allAuthors that return name & bookCount'
    
    * fix: bookCount logic returns value
    
    * feat: modify allBooks query for optional parameter author returning title by the author
    
    * feat: modify allBooks to add optional parameter genre
    
    * chore: add new deps uuid
    
    * feat: implement mutation adding new book
    
    * feat: implement mutation for editing author's born field
    
    ---------
    
    Co-authored-by: mrae43 <khaelsm.mrae43@hmail.com>

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('@apollo/server');
 const { startStandaloneServer } = require('@apollo/server/standalone');
+const { v1: uuid } = require('uuid');
 
 let authors = [
 	{
@@ -90,17 +91,82 @@ let books = [
   you can remove the placeholder query once your first one has been implemented 
 */
 
-const typeDefs = /* GraphQL */ `
-	type Query {
-		bookCount: Int!
-		authorCount: Int!
-	}
-`;
+const typeDefs =
+	/* GraphQL */
+	`
+		type Book {
+			title: String!
+			author: String!
+			published: Int!
+			genres: [String!]!
+		}
+
+		type Author {
+			name: String!
+			born: Int
+			bookCount: Int!
+		}
+
+		type Query {
+			bookCount: Int!
+			authorCount: Int!
+			allBooks(author: String, genre: String): [Book!]!
+			allAuthors: [Author!]!
+		}
+
+		type Mutation {
+			addBook(
+				title: String!
+				author: String!
+				published: Int!
+				genres: [String!]!
+			): Book
+
+			editAuthor(name: String!, setBornTo: Int!): Author
+		}
+	`;
 
 const resolvers = {
 	Query: {
 		bookCount: () => books.length,
 		authorCount: () => authors.length,
+		allBooks: (root, args) => {
+			let filteredBooks = books;
+			if (args.author) {
+				filteredBooks = filteredBooks.filter((b) => b.author === args.author);
+			}
+			if (args.genre) {
+				filteredBooks = filteredBooks.filter((b) =>
+					b.genres.includes(args.genre),
+				);
+			}
+			return filteredBooks;
+		},
+		allAuthors: () => authors,
+	},
+
+	Author: {
+		bookCount: (root) => {
+			return books.filter((book) => book.author === root.name).length;
+		},
+	},
+
+	Mutation: {
+		addBook: (root, args) => {
+			const book = { ...args, id: uuid() };
+			books = books.concat(book);
+			return book;
+		},
+		editAuthor: (root, args) => {
+			const author = authors.find((author) => author.name === args.name);
+			if (!author) return null;
+
+			const updatedData = { ...author, born: args.setBornTo };
+			authors = authors.map((author) =>
+				author.name === args.name ? updatedData : author,
+			);
+			return updatedData;
+		},
 	},
 };
 

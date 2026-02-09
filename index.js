7fbcad852440c1482f42f493db9214ea16492a5f
mrae43 <khaelsm.mrae43@hmail.com>
2026-02-09 19:35:49 +0800

    feat: implement mutation adding new book

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('@apollo/server');
 const { startStandaloneServer } = require('@apollo/server/standalone');
+const { v1: uuid } = require('uuid');
 
 let authors = [
 	{
@@ -111,6 +112,15 @@ const typeDefs =
 			allBooks(author: String, genre: String): [Book!]!
 			allAuthors: [Author!]!
 		}
+
+		type Mutation {
+			addBook(
+				title: String!
+				author: String!
+				published: Int!
+				genres: [String!]!
+			): Book
+		}
 	`;
 
 const resolvers = {
@@ -135,6 +145,14 @@ const resolvers = {
 	Author: {
 		bookCount: (root) => {
 			return books.filter((book) => book.author === root.name).length;
+		},
+	},
+
+	Mutation: {
+		addBook: (root, args) => {
+			const book = { ...args, id: uuid() };
+			books = books.concat(book);
+			return book;
 		},
 	},
 };

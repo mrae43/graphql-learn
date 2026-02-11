aca5df6d40e3c1b81243daf0a9a837fb1fbeeb11
mrae43 <khaelsm.mrae43@hmail.com>
2026-02-11 20:14:25 +0800

    feat: add context in the server file using jwt library for user login

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 const { ApolloServer } = require('@apollo/server');
 const { startStandaloneServer } = require('@apollo/server/standalone');
+const jwt = require('jsonwebtoken');
+const User = require('./models/User');
 
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
@@ -12,6 +14,22 @@ const startServer = (port) => {
 
 	startStandaloneServer(server, {
 		listen: { port },
+		context: async ({ req }) => {
+			const auth = req?.headers?.authorization;
+			if (!auth || auth.toLocaleLowerCase().startsWith('bearer')) {
+				return {};
+			}
+
+			const token = auth.substring(7);
+
+			try {
+				const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+				const currentUser = await User.findById(decodedToken.id);
+				return { currentUser };
+			} catch (error) {
+				return {};
+			}
+		},
 	}).then(({ url }) => {
 		console.log(`Server ready at ${url}`);
 	});

843a492baeddbe91d2b3819a0b56f787bbfbe22a
mrae43 <khaelsm.mrae43@hmail.com>
2026-02-13 14:14:27 +0800

    feat: refactor the server using subscription & express

--- src/server.js
+++ src/server.js
@@ -1,5 +1,13 @@
 const { ApolloServer } = require('@apollo/server');
 const { startStandaloneServer } = require('@apollo/server/standalone');
+const {
+	ApolloServerPluginDrainHttpServer,
+} = require('@apollo/server/plugin/drainHttpServer');
+const { expressMiddleware } = require('@as-integrations/express5');
+const cors = require('cors');
+const express = require('express');
+const { makeExecuteableSchema } = require('@graphql-tools/schema');
+const http = require('http');
 const jwt = require('jsonwebtoken');
 const User = require('./models/User');
 
@@ -15,22 +23,33 @@ const getUserFromAuthHeader = async (auth) => {
 	return User.findById(decodedToken.id);
 };
 
-const startServer = (port) => {
+const startServer = async (port) => {
+	const app = express();
+	const httpServer = http.createServer(app);
+
 	const server = new ApolloServer({
-		typeDefs,
-		resolvers,
+		schema: makeExecuteableSchema({ typeDefs, resolvers }),
+		plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 	});
 
-	startStandaloneServer(server, {
-		listen: { port },
-		context: async ({ req }) => {
-			const auth = req.headers.authorization;
-			const currentUser = await getUserFromAuthHeader(auth);
-			return { currentUser };
-		},
-	}).then(({ url }) => {
-		console.log(`Server ready at ${url}`);
-	});
+	await server.start();
+
+	app.use(
+		'/',
+		cors(),
+		express.json(),
+		expressMiddleware(server, {
+			context: async ({ req }) => {
+				const auth = req.headers.authorization;
+				const currrentUser = getUserFromAuthHeader(auth);
+				return { currrentUser };
+			},
+		}),
+	);
+
+	httpServer.listen(port, () =>
+		console.log(`Server is running on http://localhost:${port}`),
+	);
 };
 
 module.exports = startServer;
